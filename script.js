9fdfd9777c5c04b36f4ca3ebee7768cbec2454e7
mailtsjp <mailtsjp@gmail.com>
2020-12-31 23:21:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
         {
             name: 'Magnemite',
             location: {
-                lat: 44.496470,
-                lng: 11.320180,
+                lat: 1.301180,
+                lng: 103.899220,
             }
         },
     ];
