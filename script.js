46da37aed05451812e9823ee528086cd09698f9e
mailtsjp <mailtsjp@gmail.com>
2021-01-01 10:14:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '?';
+    button.innerText = 'Gift';
 
     let places = staticLoadPlaces();
     renderPlaces(places);
