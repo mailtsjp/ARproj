7798f0d9a01e9eb7914c6352763b92cbfdaa0b6d
mailtsjp <mailtsjp@gmail.com>
2021-01-02 17:04:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ var models = [
     
     {
         url: './assets/gift_box/scene.gltf',
-        scale: '0.05 0.05 0.05',
         info: 'Gift box, Lv. 5, HP 10/10',
+        scale: '0.05 0.05 0.05',        
         rotation: '0 90 0',
         position: '1 1 20',
     },
@@ -41,7 +41,7 @@ var models = [
     {
         url: './assets/gift/scene.gltf',
         info: 'gift, Lv. 5, HP 10/10',
-        scale: '0.50 0.50 0.50'
+        scale: '0.50 0.50 0.50',
         rotation: '0 0 0',
         position: '1 1 20',
         
