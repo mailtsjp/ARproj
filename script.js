c1d90947b0a989e2f575880342d362af6acf4d47
mailtsjp <mailtsjp@gmail.com>
2021-01-03 20:23:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]');
-    button.innerText = 'v7';
+    button.innerText = 'v8';
 //--------------------
       //showPosition();
    if(navigator.geolocation) {
