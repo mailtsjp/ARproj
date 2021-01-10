d2c444929ef8fb3624a6bc31f88626c4c4e77bc8
mailtsjp <mailtsjp@gmail.com>
2021-01-10 16:45:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -79,7 +79,7 @@ var models = [
     {   
         //esta
         Lat:' 1.3027636',
-        Long: '103.8996186,
+        Long: '103.8996186',
         url: './assets/gift_box/scene.gltf',
         scale: '0.08 0.08 0.08',
         rotation: '0 180 0',
