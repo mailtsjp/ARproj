465f2c96152f2600dc05a5a9854ba3e504146ee7
mailtsjp <mailtsjp@gmail.com>
2021-01-10 16:59:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const button = document.querySelector('button[data-action="change"]'); 
-    button.innerText = 'vX8';
+    button.innerText = 'vX9';
 //--------------------
     var Array = [];
 
